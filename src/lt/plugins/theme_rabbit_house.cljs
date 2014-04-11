dab211ec768d3c827346680d12f377e90d6a7269
kurogelee <kurogelee@users.noreply.github.com>
2014-04-12 01:24:09 +0900

    ショートカットキーによる不具合を改善
    remove-tag-behaviorsを追加
    plugin.ednのnameを変更

--- src/lt/plugins/theme_rabbit_house.cljs
+++ src/lt/plugins/theme_rabbit_house.cljs
@@ -1,7 +1,9 @@
 (ns lt.plugins.theme-rabbit-house
   (:require [lt.object :as object]
             [lt.objs.tabs :as tabs]
-            [lt.objs.command :as cmd])
+            [lt.objs.command :as cmd]
+            [lt.objs.style :as style]
+            [lt.objs.settings :as settings])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
 (def plugin-name "theme-rabbit-house")
@@ -14,7 +16,13 @@
                   :hidden true
                   :desc (str plugin-name ": set skin " skin)
                   :exec #(do
-                           (object/tag-behaviors :app [(list :lt.objs.style/set-skin skin)])
-                           (object/tag-behaviors :editor [(list :lt.objs.style/set-theme theme)]))})))
+                           (let [cur-skin (:skin @style/styles)]
+                             (when-not (= skin cur-skin)
+                               (object/remove-tag-behaviors :app [(list :lt.objs.style/set-skin cur-skin)])
+                               (object/tag-behaviors :app [(list :lt.objs.style/set-skin skin)])))
+                           (let [cur-theme (:theme @style/styles)]
+                             (when-not (= theme cur-theme)
+                               (object/remove-tag-behaviors :editor [(list :lt.objs.style/set-theme cur-theme)])
+                               (object/tag-behaviors :editor [(list :lt.objs.style/set-theme theme)]))))})))
 
 (doseq [n (range 1 6)] (skin-command n))
--- theme_rabbit_house_compiled.js
+++ theme_rabbit_house_compiled.js
@@ -1,58 +1,71 @@
 if(!lt.util.load.provided_QMARK_('lt.plugins.theme-rabbit-house')) {
 goog.provide('lt.plugins.theme_rabbit_house');
 goog.require('cljs.core');
-goog.require('lt.objs.command');
-goog.require('lt.objs.command');
+goog.require('lt.objs.settings');
 goog.require('lt.objs.tabs');
-goog.require('lt.objs.tabs');
+goog.require('lt.objs.style');
+goog.require('lt.objs.command');
+goog.require('lt.objs.settings');
+goog.require('lt.objs.style');
 goog.require('lt.object');
 goog.require('lt.object');
+goog.require('lt.objs.tabs');
+goog.require('lt.objs.command');
 lt.plugins.theme_rabbit_house.plugin_name = "theme-rabbit-house";
 lt.plugins.theme_rabbit_house.theme = "rabbit-house-light";
 lt.plugins.theme_rabbit_house.base_name = "rabbit-house-light";
-lt.plugins.theme_rabbit_house.skin_command = (function skin_command(name){var skin = [cljs.core.str(lt.plugins.theme_rabbit_house.base_name),cljs.core.str("-"),cljs.core.str(name)].join('');return lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),cljs.core.keyword.call(null,[cljs.core.str("set-skin-"),cljs.core.str(skin)].join('')),new cljs.core.Keyword(null,"hidden","hidden",4091384092),true,new cljs.core.Keyword(null,"desc","desc",1016984067),[cljs.core.str(lt.plugins.theme_rabbit_house.plugin_name),cljs.core.str(": set skin "),cljs.core.str(skin)].join(''),new cljs.core.Keyword(null,"exec","exec",1017031683),(function (){lt.object.tag_behaviors.call(null,new cljs.core.Keyword(null,"app","app",1014001043),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core._conj.call(null,cljs.core._conj.call(null,cljs.core.List.EMPTY,skin),new cljs.core.Keyword("lt.objs.style","set-skin","lt.objs.style/set-skin",3308326384))], null));
+lt.plugins.theme_rabbit_house.skin_command = (function skin_command(name){var skin = [cljs.core.str(lt.plugins.theme_rabbit_house.base_name),cljs.core.str("-"),cljs.core.str(name)].join('');return lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),cljs.core.keyword.call(null,[cljs.core.str("set-skin-"),cljs.core.str(skin)].join('')),new cljs.core.Keyword(null,"hidden","hidden",4091384092),true,new cljs.core.Keyword(null,"desc","desc",1016984067),[cljs.core.str(lt.plugins.theme_rabbit_house.plugin_name),cljs.core.str(": set skin "),cljs.core.str(skin)].join(''),new cljs.core.Keyword(null,"exec","exec",1017031683),(function (){var cur_skin_8281 = new cljs.core.Keyword(null,"skin","skin",1017436399).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.style.styles));if(cljs.core._EQ_.call(null,skin,cur_skin_8281))
+{} else
+{lt.object.remove_tag_behaviors.call(null,new cljs.core.Keyword(null,"app","app",1014001043),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core._conj.call(null,cljs.core._conj.call(null,cljs.core.List.EMPTY,cur_skin_8281),new cljs.core.Keyword("lt.objs.style","set-skin","lt.objs.style/set-skin",3308326384))], null));
+lt.object.tag_behaviors.call(null,new cljs.core.Keyword(null,"app","app",1014001043),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core._conj.call(null,cljs.core._conj.call(null,cljs.core.List.EMPTY,skin),new cljs.core.Keyword("lt.objs.style","set-skin","lt.objs.style/set-skin",3308326384))], null));
+}
+var cur_theme = new cljs.core.Keyword(null,"theme","theme",1124231483).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.style.styles));if(cljs.core._EQ_.call(null,lt.plugins.theme_rabbit_house.theme,cur_theme))
+{return null;
+} else
+{lt.object.remove_tag_behaviors.call(null,new cljs.core.Keyword(null,"editor","editor",4001043679),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core._conj.call(null,cljs.core._conj.call(null,cljs.core.List.EMPTY,cur_theme),new cljs.core.Keyword("lt.objs.style","set-theme","lt.objs.style/set-theme",1439387910))], null));
 return lt.object.tag_behaviors.call(null,new cljs.core.Keyword(null,"editor","editor",4001043679),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core._conj.call(null,cljs.core._conj.call(null,cljs.core.List.EMPTY,lt.plugins.theme_rabbit_house.theme),new cljs.core.Keyword("lt.objs.style","set-theme","lt.objs.style/set-theme",1439387910))], null));
+}
 })], null));
 });
-var seq__8267_8281 = cljs.core.seq.call(null,cljs.core.range.call(null,1,6));var chunk__8268_8282 = null;var count__8269_8283 = 0;var i__8270_8284 = 0;while(true){
-if((i__8270_8284 < count__8269_8283))
-{var n_8285 = cljs.core._nth.call(null,chunk__8268_8282,i__8270_8284);lt.plugins.theme_rabbit_house.skin_command.call(null,n_8285);
+var seq__8267_8282 = cljs.core.seq.call(null,cljs.core.range.call(null,1,6));var chunk__8268_8283 = null;var count__8269_8284 = 0;var i__8270_8285 = 0;while(true){
+if((i__8270_8285 < count__8269_8284))
+{var n_8286 = cljs.core._nth.call(null,chunk__8268_8283,i__8270_8285);lt.plugins.theme_rabbit_house.skin_command.call(null,n_8286);
 {
-var G__8286 = seq__8267_8281;
-var G__8287 = chunk__8268_8282;
-var G__8288 = count__8269_8283;
-var G__8289 = (i__8270_8284 + 1);
-seq__8267_8281 = G__8286;
-chunk__8268_8282 = G__8287;
-count__8269_8283 = G__8288;
-i__8270_8284 = G__8289;
+var G__8287 = seq__8267_8282;
+var G__8288 = chunk__8268_8283;
+var G__8289 = count__8269_8284;
+var G__8290 = (i__8270_8285 + 1);
+seq__8267_8282 = G__8287;
+chunk__8268_8283 = G__8288;
+count__8269_8284 = G__8289;
+i__8270_8285 = G__8290;
 continue;
 }
 } else
-{var temp__4092__auto___8290 = cljs.core.seq.call(null,seq__8267_8281);if(temp__4092__auto___8290)
-{var seq__8267_8291__$1 = temp__4092__auto___8290;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8267_8291__$1))
-{var c__7561__auto___8292 = cljs.core.chunk_first.call(null,seq__8267_8291__$1);{
-var G__8293 = cljs.core.chunk_rest.call(null,seq__8267_8291__$1);
-var G__8294 = c__7561__auto___8292;
-var G__8295 = cljs.core.count.call(null,c__7561__auto___8292);
-var G__8296 = 0;
-seq__8267_8281 = G__8293;
-chunk__8268_8282 = G__8294;
-count__8269_8283 = G__8295;
-i__8270_8284 = G__8296;
+{var temp__4092__auto___8291 = cljs.core.seq.call(null,seq__8267_8282);if(temp__4092__auto___8291)
+{var seq__8267_8292__$1 = temp__4092__auto___8291;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8267_8292__$1))
+{var c__7561__auto___8293 = cljs.core.chunk_first.call(null,seq__8267_8292__$1);{
+var G__8294 = cljs.core.chunk_rest.call(null,seq__8267_8292__$1);
+var G__8295 = c__7561__auto___8293;
+var G__8296 = cljs.core.count.call(null,c__7561__auto___8293);
+var G__8297 = 0;
+seq__8267_8282 = G__8294;
+chunk__8268_8283 = G__8295;
+count__8269_8284 = G__8296;
+i__8270_8285 = G__8297;
 continue;
 }
 } else
-{var n_8297 = cljs.core.first.call(null,seq__8267_8291__$1);lt.plugins.theme_rabbit_house.skin_command.call(null,n_8297);
+{var n_8298 = cljs.core.first.call(null,seq__8267_8292__$1);lt.plugins.theme_rabbit_house.skin_command.call(null,n_8298);
 {
-var G__8298 = cljs.core.next.call(null,seq__8267_8291__$1);
-var G__8299 = null;
-var G__8300 = 0;
+var G__8299 = cljs.core.next.call(null,seq__8267_8292__$1);
+var G__8300 = null;
 var G__8301 = 0;
-seq__8267_8281 = G__8298;
-chunk__8268_8282 = G__8299;
-count__8269_8283 = G__8300;
-i__8270_8284 = G__8301;
+var G__8302 = 0;
+seq__8267_8282 = G__8299;
+chunk__8268_8283 = G__8300;
+count__8269_8284 = G__8301;
+i__8270_8285 = G__8302;
 continue;
 }
 }
